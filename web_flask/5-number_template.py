48d7d6d69c47b0a489fa1bd354d9bffc8a9c93a5
Danielxxdxdxd <xxdxdxd332@gmail.com>
2022-12-08 10:20:23 -0500

    Number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,52 +1,59 @@
 #!/usr/bin/python3
-"""This module starts a Flask web application and displays an integer in HTML
 """
-from flask import Flask
-from flask import escape
-from flask import render_template
+This script display “Hello HBNB!” on '/' and
+“HBNB” on /hbnb route
+"C <text>" on /c/ rourte and the text variable
+"""
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_world():
-    """Returns the string Hello HBNB!"""
-    return 'Hello HBNB!'
+@app.route('/')
+def main_page():
+    """Return “Hello HBNB!” on the console"""
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hello_hbnb():
-    """Returns the string HBNB"""
-    return 'HBNB'
+@app.route('/hbnb')
+def hbnb():
+    """Return "HBNB" on the console on /hbnb"""
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def hello_c(text):
-    """Returns a string starting with C"""
-    mod_text = escape(text)
-    return 'C %s' % mod_text.replace("_", " ")
+@app.route('/c/<text>')
+def display_text(text):
+    """Return "C" + <text> variable on /c/"""
+    new_string = text
+    if '_' in new_string:
+        new_string = text.replace('_', ' ')
+    return 'C {}'.format(new_string)
 
 
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def hello_python(text="is cool"):
-    """Returns a string starting with Python"""
-    mod_text = escape(text)
-    return 'Python %s' % mod_text.replace("_", " ")
+@app.route('/python/', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def display_text_default(text):
+    """Return "Python" with the default value is cool"""
+    new_string = text
+    if '_' in new_string:
+        new_string = text.replace('_', ' ')
+    return 'Python {}'.format(new_string)
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def hello_int(n):
-    """Returns a number"""
-    return '%d is a number' % n
+@app.route('/number/<int:n>')
+def display_int(n):
+    """Return n is a number only if n is type int"""
+    if type(n) is int:
+        return '{:d} is a number'.format(n)
 
 
-# @app.route('/number_template/', strict_slashes=False)
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    """Returns an HTML page with a number"""
-    return render_template('5-number.html', n=n)
+@app.route('/number_template/<int:n>')
+def display_html(n):
+    """Display a html page only if n is type int"""
+    if type(n) is int:
+        return render_template('5-number.html', n=n)
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
