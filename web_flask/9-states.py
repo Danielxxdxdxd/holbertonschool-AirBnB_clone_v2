1af08cd9706b01448548b4585af42c8ecc4d8997
Danielxxdxdxd <xxdxdxd332@gmail.com>
2022-12-08 18:30:39 -0500

    States and State

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,35 +1,34 @@
 #!/usr/bin/python3
-"""This module starts a Flask web application state and cities by state id
-"""
-from flask import Flask
-from flask import escape
-from flask import render_template
+"""use flask with storage and db storage"""
 from models import storage
-from models import State
+from models.state import State
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<id>', strict_slashes=False)
-def display_states_and_cities(id=None):
-    """displays states and cities by state id"""
-    state_dict = storage.all(State)
-    found = None
-    if id is not None:
-        for state in state_dict.values():
-            if state.id == id:
-                found = state
+@app.route("/states", strict_slashes=False)
+def state_list():
+    """display list of states"""
+    state = storage.all(State)
+    return render_template("9-states.html", states=state, mode="state")
 
-    return render_template('9-states.html', state_dict=state_dict, id=id,
-                           found=found)
+
+@app.route("/states/<id>")
+def states_id(id):
+    """display id for the states"""
+    for state in storage.all(State).values():
+        if state.id == id:
+            return render_template("9-states.html", states=state,
+                                   mode="state_id")
+    return render_template("9-states.html", states=state, mode="none")
 
 
 @app.teardown_appcontext
-def teardown_db(self):
-    """removes the current SQLAlchemy Session"""
-    storage.close()
+def teardown(execption):
+    """close storage"""
+    return storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
